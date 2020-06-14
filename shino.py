6087f68d1cdaad796a9c6907fef2e94caba0f81b
Abhishek Bhattacharya <abhbhat972gmail.com>
2020-06-14 23:26:16 +0530

    Added 2 new commands and changed one

--- shino.py
+++ shino.py
@@ -7,25 +7,43 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await client.change_presence(activity=discord.Activity(name='my dudes', type=discord.ActivityType.watching))
+    await client.change_presence(activity=discord.Activity(name='the matrix', type=discord.ActivityType.watching))
 
 @client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
 
+	if message.content.startswith('>>help'):
+		await message.channel.send('''Hi! Thanks for using Shinobot. This is a quirky and sometimes useful bot made by Shinoda. 
+
+Here's its commands:
+```>>help - bot info
+>>ping - if bot responds, its alive
+>>game - bot selects a random game from dSolver's incremental games plaza
+>>sing - bot sings shino's favorite incremental game song
+>>fav - bot talks about the idle games that Shino remembers fondly```''')
+
 	if message.content.startswith('>>ping'):
 		await message.channel.send('pong!')
 
-	if 'bitburner' in message.content:
+	if 'bitburner' in message.content.lower():
 		await message.channel.send('Bitburner is sooo good!')
 
-	'''if '4G' in message.content:
-		await message.add_reaction(emoji='\U0000FE0F')
-		await message.add_reaction(emoji='\U0001F1EC')'''
+	if message.content.startswith(">>fav"):
+		await message.channel.send('''Shino has played many incrmental games but some of them stick out in his memory more than others. 
 
-	if message.content.startswith('>>song'):
-		await message.channel.send('https://timewastinggames.bandcamp.com/album/suicidle-ost')
+They are (in no particular order):
+```Infinite Hero (no longer playable)
+Slurpy Derpy (Classic)
+Realm Grinder
+Zombidle
+NGU
+Bitburner
+	```''')
+
+	if message.content.startswith('>>sing'):
+		await message.channel.send('https://timewastinggames.bandcamp.com/track/life-is-a-countdown')
 
 	if message.content.startswith('>>game'):
 		async with message.channel.typing():
