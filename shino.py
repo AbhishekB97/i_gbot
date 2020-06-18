99d3fd6a4d21220933e726f48242a0cc1c7682aa
Abhishek Bhattacharya <abhbhat972gmail.com>
2020-06-18 07:02:19 +0530

    Refactored code, added some hidden commands and triggers

--- shino.py
+++ shino.py
@@ -1,21 +1,30 @@
 import discord
 import config
 import aiohttp
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='>>', help_command=None, activity=discord.Activity(name='the matrix', type=discord.ActivityType.watching))
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    await client.change_presence(activity=discord.Activity(name='the matrix', type=discord.ActivityType.watching))
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-	if message.author == client.user:
+	if message.author == bot.user:
 		return
 
-	if message.content.startswith('>>help'):
-		await message.channel.send('''Hi! Thanks for using Shinobot. This is a quirky and sometimes useful bot made by Shinoda. 
+	if 'bitburner' in message.content.lower():
+		await message.channel.send('Bitburner is sooo good!')
+
+	if 'doom' in message.content.lower():
+		await message.channel.send('**Dooooooom**')
+
+	await bot.process_commands(message)
+
+@bot.command()
+async def help(ctx):
+	await ctx.send('''Hi! Thanks for using Shinobot. This is a quirky and sometimes useful bot made by Shinoda. 
 
 Here's its commands:
 ```>>help - bot info
@@ -24,14 +33,9 @@ Here's its commands:
 >>sing - bot sings shino's favorite incremental game song
 >>fav - bot talks about the idle games that Shino remembers fondly```''')
 
-	if message.content.startswith('>>ping'):
-		await message.channel.send('pong!')
-
-	if 'bitburner' in message.content.lower():
-		await message.channel.send('Bitburner is sooo good!')
-
-	if message.content.startswith(">>fav"):
-		await message.channel.send('''Shino has played many incrmental games but some of them stick out in his memory more than others. 
+@bot.command()
+async def fav(ctx):
+	await ctx.send('''Shino has played many incrmental games but some of them stick out in his memory more than others. 
 
 They are (in no particular order):
 ```Infinite Hero (no longer playable)
@@ -42,20 +46,30 @@ NGU
 Bitburner
 	```''')
 
-	if message.content.startswith('>>sing'):
-		await message.channel.send('https://timewastinggames.bandcamp.com/track/life-is-a-countdown')
+@bot.command()
+async def ping(ctx):
+	await ctx.send('pong!')
 
-	if message.content.startswith('>>game'):
-		async with message.channel.typing():
+@bot.command()
+async def pong(ctx):
+	await ctx.send("That's my line :rolling_eyes:")
+
+@bot.command()
+async def sing(ctx):
+	await ctx.send('https://timewastinggames.bandcamp.com/track/life-is-a-countdown')
+
+@bot.command()
+async def game(ctx):
+	async with ctx.channel.typing():
 			BASE_API_URL = "https://plaza.dsolver.ca/api/games"
 			EMBED_QUERY = '&customFields=slug,name,link,shortDescription,logo'
 
 			async with aiohttp.ClientSession() as session:
 				async with session.get(f'{BASE_API_URL}?random=true{EMBED_QUERY}') as resp:
 					if resp.status != 200:
-						await message.channel.send("Something went wrong!  Please try again in a few moments.")
+						await ctx.send("Something went wrong!  Please try again in a few moments.")
 						return
 					rand_game, = await resp.json()  # Comma after rand_game to get unpack the list and get the dict
-				await message.channel.send(f'{message.author.mention} Try {rand_game["link"]}')
+				await ctx.send(f'{ctx.author.mention} Try {rand_game["link"]}')
 
-client.run(config.TOKEN)
+bot.run(config.TOKEN)
